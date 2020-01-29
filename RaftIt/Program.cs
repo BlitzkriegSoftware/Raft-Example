35ea280b34eef0b0bd3cdce7278fdb26e562dc8d
Stuart Williams <spookdejur@hotmail.com>
2020-01-28 17:07:27 -0800

    WIP

---
+++ RaftIt/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace RaftIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Args = args;
+            var tester = Program.Services.GetService<Workers.SingletonWorker>();
+            string id = Guid.NewGuid().ToString();
+            tester.Run(id);
+        }
+
+        #region "DI"
+
+        private static string[] Args { get; set; }
+
+        private static IServiceProvider _services;
+
+        public static IServiceProvider Services
+        {
+            get
+            {
+                if (_services == null)
+                {
+                    // Create service collection
+                    var serviceCollection = new ServiceCollection();
+
+                    // Build DI Stack inc. Logging, Configuration, and Application
+                    ConfigureServices(serviceCollection);
+
+                    // Create service provider
+                    _services = serviceCollection.BuildServiceProvider();
+                }
+                return _services;
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // Configuration
+            var configurationBuilder = new ConfigurationBuilder();
+
+            if ((Args != null) && (Args.Length > 0)) configurationBuilder.AddCommandLine(Args);
+
+            var config = configurationBuilder.Build();
+            services.AddSingleton(config);
+
+            // Logging
+            services.AddLogging(loggingBuilder => {
+                // This line must be 1st
+                loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+
+                // Console is generically cloud friendly
+                loggingBuilder.AddConsole();
+            });
+
+            // App to run
+            services.AddTransient<Workers.ISingletonWorker, Workers.SingletonWorker>();
+        }
+
+        #endregion
+
+    }
+}
---
+++ RaftIt/Workers/SingletonWorker.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+
+namespace RaftIt.Workers
+{
+    public class SingletonWorker : ISingletonWorker
+    {
+        #region "CTOR"
+        private readonly ILogger _logger;
+        private readonly IConfigurationRoot _config;
+
+        public SingletonWorker(ILogger<SingletonWorker> logger, IConfigurationRoot config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        #endregion
+
+        public void Run(string id)
+        {
+            _logger.LogInformation($"Worker {id}");
+            Thread.Sleep(this.Sleep);
+        }
+
+        #region "Config"
+
+        public const int Sleep_Default = 1000 * 60;
+
+        public int Sleep
+        {
+            get
+            {
+                string c = _config["sleepms"];
+                if (!int.TryParse(c, out int i)) i = Sleep_Default;
+                return i;
+            }
+        }
+
+        #endregion
+    }
+}
